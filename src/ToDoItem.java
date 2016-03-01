bb2ecd2cd6e060b20abef6518b7d91017fb5d50e
MarshallRich <rlmarsh@g.clemson.edu>
2016-03-01 10:38:50 -0500

    added database functionality

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -2,11 +2,13 @@
  * Created by MacLap on 2/8/16.
  */
 public class ToDoItem {
-    String text;
-    boolean isDone;
+    public String text;
+    public boolean isDone;
+    public int id;
 
-    public ToDoItem(String text, boolean isDone) {
+    public ToDoItem(int id, String text, boolean isDone) {
         this.text = text;
         this.isDone = isDone;
+        this.id = id;
     }
 }
